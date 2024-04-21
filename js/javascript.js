79387ea702c264eb44006d9f58954cc9a70650c7
MIGUEVARVAL2 <mivargasv@unal.edu.co>
2024-04-20 20:00:26 -0500

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+
+
+const toggleButton = document.getElementById('toggleButton');
+const content = document.getElementById('content');
+
+toggleButton.addEventListener('click', () => {
+    content.style.display = content.style.display === 'none' ? 'inline' : 'none';
+});
+
+const toggleButton2 = document.getElementById('toggleButton2');
+const content2 = document.getElementById('content2');
+
+toggleButton2.addEventListener('click', () => {
+    content2.style.display = content2.style.display === 'none' ? 'inline' : 'none';
+});
+
